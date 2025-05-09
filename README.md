04d18a3e48eed020030aab56fad5ec139da9f3e1
Lenny Kay <77846195+lenbot7@users.noreply.github.com>
2025-05-09 11:57:14 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# coding-clicker
+# coding clicker
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/22c-kay/pen/gbbWRNO](https://codepen.io/22c-kay/pen/gbbWRNO).
+
---
+++ src/script.js
@@ -0,0 +1,78 @@
+var code = 0; 
+var gpt = document.getElementById("img")
+var corehours = 0;
+var createdCode = 1; 
+var count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+// Storage for the previous angle
+var lastAngle = "";
+
+function spin(){ 
+  // Update the total angle needed
+  lastAngle = +lastAngle + 180;
+  
+  // For testing:
+  console.clear()
+  console.log("Current total angle: " + lastAngle);
+
+  // Move the needle:
+  gpt.style.transform = "rotate(" + lastAngle + "deg)";
+}
+ 
+
+function buttonPressed() { 
+
+code=code+createdCode; spin();spin()
+
+             document.getElementById("total").innerHTML = code;	 
+if (code%10==0&code!=0) {
+  count = code
+  fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
+} 
+function button2Pressed(){
+  if (code>=10){
+    code -= 10
+    corehours += 1
+  }
+  else{
+    alert("You dont have enough lines of code for that")
+  }
+  document.getElementById("total").innerHTML = code;	
+  document.getElementById("cash").innerHTML = corehours;	
+}
